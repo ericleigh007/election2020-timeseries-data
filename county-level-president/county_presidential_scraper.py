e513050514a102bb26196575100c212d9431c56c
d0ntw84f8 <74834882+d0ntw84f8@users.noreply.github.com>
2020-11-23 16:36:32 -0500

    Update county_presidential_scraper.py
    Added code to direct the scraper to several other urls with additional (but, mostly redundant) data.  Also fixed the code to drop duplicate rows from the final dataset.

--- county-level-president/county_presidential_scraper.py
+++ county-level-president/county_presidential_scraper.py
@@ -1,12 +1,15 @@
 import requests
 import time
+import tempfile
+from pathlib import Path
+
 import pandas as pd
 
 
-src_dir = ''
-
 def get_archived_county_results(race):
-    assert race['race_id'][2:6] == '-G-P'
+    if race['race_id'][2:6] != '-G-P':
+        return []
+    print(race['race_id'])
     
     state_records = []
     for county in race['counties']:
@@ -31,23 +34,24 @@ def get_archived_county_results(race):
         vote_record['margin2012']         = county.get('margin2012', np.nan)
         state_records.append(vote_record)
     
-    return race['state_id'], state_records
+    return state_records
 
-def extract_country_wide_county_data(csv_file_name):
-    print('Searching for archived copies of the national U.S. presidential election results')
+def load_county_presidential(nyt_url, csv_path):
+    print('Searching for archived copies of the U.S. presidential election results')
 
     archive_api_url = 'http://web.archive.org/cdx/search/cdx?url='
     archive_output_qualifier = '&output=json&from=20201104&to=20201107'   #limit search to election day +4
-    state_str = state.lower().replace(' ','-')
-    nyt_url = 'https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/national-map-page/national/president.json'
     search_hits = requests.get(archive_api_url + nyt_url + archive_output_qualifier).json()[1:]
     
     urls_searched = []
     for i, url_group in enumerate(search_hits):
-        archived_url = 'http://web.archive.org/web/' + url_group[1] + 'if_/' + url_group[2]
-        good_url_status = int(url_group[4]) < 400
-        correct_data_type = url_group[3] == 'application/json'
-        url_is_new = not archived_url in urls_searched
+        try:
+            archived_url = 'http://web.archive.org/web/' + url_group[1] + 'if_/' + url_group[2]
+            good_url_status = int(url_group[4]) < 400
+            correct_data_type = url_group[3] == 'application/json'
+            url_is_new = not archived_url in urls_searched
+        except:
+            continue
         
         if good_url_status and correct_data_type and url_is_new:
             urls_searched.append(archived_url)
@@ -60,9 +64,9 @@ def extract_country_wide_county_data(csv_file_name):
                 
                 vote_records = []
                 for race in race_data.json()['data']['races']:
-                    vote_records += get_archived_county_results(race)[1]
+                    vote_records += get_archived_county_results(race)
                 results_df = pd.DataFrame.from_records(vote_records)
-                results_df.to_csv(csv_file_name, mode='a', header=(i==0))
+                results_df.to_csv(csv_path, mode='a', header=~csv_path.is_file())
                 
                 print('Data extraction successful, took {:.1f} seconds'.format(time.time() - download_finish))
                 print('Waiting 5x length of previous download as courtesy before continuing...')
@@ -71,6 +75,46 @@ def extract_country_wide_county_data(csv_file_name):
             except Exception as e:
                 print('Failed to extract data from {} with error: {}'.format(archived_url, str(e)))
 
-extract_country_wide_county_data(src_dir + '/' + 'county_presidential.csv')
+def scrape_state_pages(csv_path):
+    states_list = ['Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 
+     'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
+     'Hawaii', 'Iowa', 'Idaho', 'Illinois', 'Indiana', 'Kansas', 'Kentucky',
+     'Louisiana', 'Massachusetts', 'Maryland', 'Maine', 'Michigan',
+     'Minnesota', 'Missouri', 'Mississippi', 'Montana', 'North Carolina',
+     'North Dakota', 'Nebraska', 'New Hampshire', 'New Jersey', 'New Mexico',
+     'Nevada', 'New York', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+     'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas',
+     'Utah', 'Virginia', 'Vermont', 'Washington', 'Wisconsin',
+     'West Virginia', 'Wyoming']
 
+    for state in states_list:
+        state_str = state.lower().replace(' ','-')
+        nyt_urlC = 'https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/state-page/{}.json'.format(state_str)
+        load_county_presidential(nyt_urlC, csv_path)
+        nyt_urlD = 'https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/{}/president.json'.format(state_str)
+        load_county_presidential(nyt_urlD, csv_path)
+    
+def scrape_national_pages(csv_path, all_pages=False):
+    nyt_urlA = 'https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/national-map-page/national/president.json'
+    nyt_urlB = 'https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/votes-remaining-page/national/president.json'
+    load_county_presidential(nyt_urlA, csv_path)
+    if all_pages:
+        load_county_presidential(nyt_urlB, csv_path)
 
+def remove_duplicates(csv_path):
+    cols_to_keep = ['state', 'county', 'fips', 'timestamp', 'last_updated', 
+                    'absentee_dem', 'absentee_rep', 'absentee_jorgensen', 'votes_dem', 
+                    'votes_rep', 'votes_jorgensen', 'absentee2020', 'votes2020', 
+                    'margin2020', 'votes2016', 'margin2016', 'votes2012', 'margin2012']
+
+    temp = tempfile.NamedTemporaryFile(prefix=csv_path.name, dir=csv_path.parent, delete=False)
+    df = pd.read_csv(csv_path)[cols_to_keep].drop_duplicates(['state', 'timestamp', 'county'], keep='last')
+    df.to_csv(temp.name)
+    Path(temp.name).replace(csv_path)
+
+if __name__=='__main__':
+    src_dir = ''   #you'll need to put the path to your data directory here
+    csv_path = Path(src_dir + '/' + 'county_presidential_test.csv')
+    scrape_national_pages(csv_path, all_pages=False)
+    scrape_state_pages(csv_path)   #<-- !!!Warning this will take ~24 hours and only net ~12k original rows!!!
+    remove_duplicates(csv_path)
